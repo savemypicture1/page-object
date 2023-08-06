0dbc8b5f18538e92c4a4facfda7a56b6b1e24b32
Serhii Taran <savemypicture1@gmail.com>
2023-08-06 17:20:13 +0300

    add tests for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
 
 
@@ -7,8 +6,10 @@ class LoginPage(BasePage):
     USER_NAME = (By.CSS_SELECTOR, "#user-name")
     PASSWORD = (By.CSS_SELECTOR, "#password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#login-button")
-
-
+    ERROR_MSG = (By.CSS_SELECTOR, "h3")
+    ERROR_MSGS = {"invalid_credentials": "Epic sadface: Username and password do not match any user in this service",
+                  "locked_user": "Epic sadface: Sorry, this user has been locked out.",
+                  "without_credentials": "Epic sadface: Username is required"}
 
 
     def click_login_button(self):
@@ -18,14 +19,10 @@ class LoginPage(BasePage):
     def enter_username(self, username):
         self.send_keys(self.USER_NAME, username)
 
+
     def enter_password(self, password):
         self.send_keys(self.PASSWORD, password)
 
-    # def find_username_field(self):
-    #     return self.find(self.USER_NAME)
-    #
-    # def find_password_field(self):
-    #     return self.find(self.PASSWORD)
 
-
-
+    def find_error_msg(self):
+        return self.find(self.ERROR_MSG)
